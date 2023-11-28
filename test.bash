5c0fe5d97cd1331d9a477454e64dcf02b38d5fe0
Kou Suzuki <pinoniq123@gmail.com>
2023-11-28 19:46:54 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -2,17 +2,29 @@
 # SPDX-FileCopyrightText: 2023 Kou Suzuki
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-	echo NG at Line $1
-	res=1
+
+ng() {
+    echo "NG at Line $1"
+    res=1
 }
 
 res=0
 
-out=$(seq 5| ./plus)
-Expected_out=15.0 5 3.0
- "${out}" = "${Expected_out}" ] || ng ${LINENO}
+# テスト対象のプログラムを呼び出して出力を取得
+out=$(seq 5 | ./plus)
 
-[ "$res" = 0 ] && echo OK
-exit $[res]
+# 期待される出力
+expected_output="15.0 5 3.0"
+
+# 出力が期待される結果と一致するか確認
+if [ "$out" != "$expected_output" ]; then
+    ng ${LINENO}
+fi
+
+# エラーがなければOKを表示
+[ "$res" = 0 ] && echo "OK"
+
+# テスト結果を終了コードとして返す
+exit $res
+
 
